05822b4a1bba4ca16c2d0ea02cf87b4d1bba18e4
Abdul Ahad <ahadking6152@gmail.com>
2025-07-31 10:26:20 -0700

    Update layout.tsx with analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -132,8 +132,13 @@ export default function RootLayout({
         <script
           defer
           data-domain="portfolio-prei.vercel.app"
-          src="https://plausible.io/js/script.js"
+          src="https://plausible.io/js/script.file-downloads.outbound-links.pageview-props.tagged-events.js"
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`,
+          }}
+        />
       </Head>
       {/*
         FIXED: Added suppressHydrationWarning to the body tag.
